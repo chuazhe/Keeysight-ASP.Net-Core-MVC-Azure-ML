012955bfca972ecac9ee0012a86917990466fc43
Chua Wei Zhe <chuazhe@gmail.com>
2019-04-23 16:12:40 +0800

    Minor Change

--- Keeysight/wwwroot/js/site.js
+++ Keeysight/wwwroot/js/site.js
@@ -3,3 +3,15 @@ $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+/* Menu Click Script*/
+$('.list-group-item').on('click', function () {
+    var $this = $(this);
+    //var $alias = $this.data('alias');
+
+    $('.active').removeClass('active');
+    $this.toggleClass('active')
+
+    // Pass clicked link element to another function
+    //myfunction($this, $alias)
+})
